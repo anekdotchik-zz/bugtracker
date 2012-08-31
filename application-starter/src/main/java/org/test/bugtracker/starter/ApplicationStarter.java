72e0cbf09e25b5716cdd92e2e781b3998f3aa90f
Anekdotchik <anekdotchik@gmail.com>
2012-09-01 00:30:33 +0300

    Minor fix

--- application-starter/src/main/java/org/test/bugtracker/starter/ApplicationStarter.java
+++ application-starter/src/main/java/org/test/bugtracker/starter/ApplicationStarter.java
@@ -1,13 +1,17 @@
 package org.test.bugtracker.starter;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class ApplicationStarter {
-
-    /**
-     * @param args
-     */
+    private ApplicationContext context;
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        new ApplicationStarter().start();
+    }
+    
+    public void start() {
+        context = new ClassPathXmlApplicationContext("spring/context.xml");
+        context.toString();
     }
 
 }
