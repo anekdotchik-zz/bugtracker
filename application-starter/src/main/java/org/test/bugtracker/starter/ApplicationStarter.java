4483c28aa2b02f9227d3277d2c04a0d4d38f9635
Anekdotchik <anekdotchik@gmail.com>
2012-08-31 23:57:16 +0300

    Add basic starter

---
+++ application-starter/src/main/java/org/test/bugtracker/starter/ApplicationStarter.java
@@ -0,0 +1,13 @@
+package org.test.bugtracker.starter;
+
+public class ApplicationStarter {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
