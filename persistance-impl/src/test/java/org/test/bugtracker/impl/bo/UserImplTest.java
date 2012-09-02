7b7fa8af03ad0bf5e4e950f5387fe5b4b94e31d2
Anekdotchik <anekdotchik@gmail.com>
2012-09-02 23:20:58 +0300

    Add user test

--- persistance-impl/src/test/java/org/test/bugtracker/impl/bo/UserImplTest.java
+++ persistance-impl/src/test/java/org/test/bugtracker/impl/bo/UserImplTest.java
@@ -3,6 +3,7 @@ package org.test.bugtracker.impl.bo;
 import static junit.framework.Assert.*;
 
 import org.testng.annotations.Test;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,6 +14,9 @@ import org.test.bugtracker.model.User;
 
 @ContextConfiguration(locations = { "classpath:spring/context.xml" })
 public class UserImplTest extends AbstractTestNGSpringContextTests {
+    private static final String PASS = "pass";
+    private static final long ID = 1L;
+    private static final String LOGIN = "login";
     @Autowired
     private ApplicationContext context;
     @Autowired
@@ -21,25 +25,55 @@ public class UserImplTest extends AbstractTestNGSpringContextTests {
     @Test(priority=0)
     public void createNewUserTest() {
         User user = new UserImpl();
-        user.setLogin("login");
-        user.setPass("pass");
+        user.setLogin(LOGIN);
+        user.setPass(PASS);
         userBO.save(user);
-        assertTrue(user.getId() == 1L);
+        assertTrue(user.getId() == ID);
     }
     
     @Test(dependsOnMethods="createNewUserTest")
     public void findUserTestById() {
         User user = userBO.findById(1L);
         assertNotNull(user);
-        assertEquals("login", user.getLogin());
-        assertEquals("pass", user.getPass());
+        assertEquals(LOGIN, user.getLogin());
+        assertEquals(PASS, user.getPass());
     }
     
     @Test(dependsOnMethods="createNewUserTest")
     public void findUserTestByLogin() {
-        User user = userBO.findByLogin("login");
+        User user = userBO.findByLogin(LOGIN);
         assertNotNull(user);
-        assertEquals("login", user.getLogin());
-        assertEquals("pass", user.getPass());
-    }    
+        assertEquals(LOGIN, user.getLogin());
+        assertEquals(PASS, user.getPass());
+    }
+    
+    @Test(dependsOnMethods="createNewUserTest", expectedExceptions={ConstraintViolationException.class})
+    public void createUserWithSomeIdTest() {
+        User user = new UserImpl();
+        user.setId(1L);
+        user.setLogin(LOGIN);
+        user.setPass(PASS);
+        userBO.save(user);
+    }
+    
+    @Test(dependsOnMethods="createNewUserTest", expectedExceptions={ConstraintViolationException.class})
+    public void createUserWithSomeLoginTest() {
+        User user = new UserImpl();
+        user.setLogin(LOGIN);
+        user.setPass(PASS);
+        userBO.save(user);
+    }
+
+    @Test(dependsOnMethods="createNewUserTest")
+    public void updateUserLoginTest() {
+        User user = userBO.findById(1L);
+        user.setLogin(LOGIN+"2");
+        user.setPass(PASS+"2");
+        userBO.update(user);
+        User user2 = userBO.findById(1L);
+        assertNotNull(user2);
+        assertEquals(LOGIN+"2", user2.getLogin());
+        assertEquals(PASS+"2", user2.getPass());
+    }
+
 }
