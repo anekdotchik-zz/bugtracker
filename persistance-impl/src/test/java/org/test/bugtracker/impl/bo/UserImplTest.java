f1e736bcc6c3d5bdf525bd835e1cfd4323b67217
Anekdotchik <anekdotchik@gmail.com>
2012-09-02 23:59:21 +0300

    Add some test methods

--- persistance-impl/src/test/java/org/test/bugtracker/impl/bo/UserImplTest.java
+++ persistance-impl/src/test/java/org/test/bugtracker/impl/bo/UserImplTest.java
@@ -2,7 +2,7 @@ package org.test.bugtracker.impl.bo;
 
 import static junit.framework.Assert.*;
 
-import org.testng.annotations.Test;
+import org.hibernate.PropertyValueException;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -11,6 +11,7 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.test.bugtracker.bo.UserBO;
 import org.test.bugtracker.impl.model.UserImpl;
 import org.test.bugtracker.model.User;
+import org.testng.annotations.Test;
 
 @ContextConfiguration(locations = { "classpath:spring/context.xml" })
 public class UserImplTest extends AbstractTestNGSpringContextTests {
@@ -23,7 +24,7 @@ public class UserImplTest extends AbstractTestNGSpringContextTests {
     private UserBO userBO;
 
     @Test(priority = 0)
-    public void createNewUserTest() {
+    public void createNewUser() {
         User user = new UserImpl();
         user.setLogin(LOGIN);
         user.setPass(PASS);
@@ -31,24 +32,25 @@ public class UserImplTest extends AbstractTestNGSpringContextTests {
         assertTrue(user.getId() == ID);
     }
 
-    @Test(priority = 1, dependsOnMethods = "createNewUserTest")
-    public void findUserTestById() {
+    @Test(priority = 1, dependsOnMethods = "createNewUser")
+    public void findUserById() {
         User user = userBO.findById(1L);
         assertNotNull(user);
         assertEquals(LOGIN, user.getLogin());
         assertEquals(PASS, user.getPass());
     }
 
-    @Test(priority = 1, dependsOnMethods = "createNewUserTest")
-    public void findUserTestByLogin() {
+    @Test(priority = 1, dependsOnMethods = "createNewUser")
+    public void findUserByLogin() {
         User user = userBO.findByLogin(LOGIN);
         assertNotNull(user);
         assertEquals(LOGIN, user.getLogin());
         assertEquals(PASS, user.getPass());
     }
 
-    @Test(priority = 1, dependsOnMethods = "createNewUserTest", expectedExceptions = { ConstraintViolationException.class })
-    public void createUserWithSomeIdTest() {
+    @Test(priority = 1, dependsOnMethods = "createNewUser",
+            expectedExceptions = { ConstraintViolationException.class })
+    public void createUserWithSomeId() {
         User user = new UserImpl();
         user.setId(1L);
         user.setLogin(LOGIN);
@@ -56,16 +58,33 @@ public class UserImplTest extends AbstractTestNGSpringContextTests {
         userBO.save(user);
     }
 
-    @Test(priority = 1, dependsOnMethods = "createNewUserTest", expectedExceptions = { ConstraintViolationException.class })
-    public void createUserWithSomeLoginTest() {
+    @Test(priority = 1, dependsOnMethods = "createNewUser",
+            expectedExceptions = { PropertyValueException.class })
+    public void createUserWithoutLogin() {
+        User user = new UserImpl();
+        user.setPass(PASS);
+        userBO.save(user);
+    }
+
+    @Test(priority = 1, dependsOnMethods = "createNewUser",
+            expectedExceptions = { PropertyValueException.class })
+    public void createUserWithoutPass() {
+        User user = new UserImpl();
+        user.setLogin(LOGIN);
+        userBO.save(user);
+    }
+    
+    @Test(priority = 1, dependsOnMethods = "createNewUser",
+            expectedExceptions = { ConstraintViolationException.class })
+    public void createUserWithSomeLogin() {
         User user = new UserImpl();
         user.setLogin(LOGIN);
         user.setPass(PASS);
         userBO.save(user);
     }
 
-    @Test(priority = 2, dependsOnMethods = "createNewUserTest")
-    public void updateUserLoginAndPassTest() {
+    @Test(priority = 2, dependsOnMethods = "createNewUser")
+    public void updateUserLoginAndPass() {
         User user = userBO.findById(1L);
         user.setLogin(LOGIN + "2");
         user.setPass(PASS + "2");
@@ -77,8 +96,8 @@ public class UserImplTest extends AbstractTestNGSpringContextTests {
         assertTrue(ID == user2.getId());
     }
 
-    @Test(priority = 3, dependsOnMethods = "createNewUserTest")
-    public void deleteUserTest() {
+    @Test(priority = 3, dependsOnMethods = "createNewUser")
+    public void deleteUser() {
         User user = userBO.findById(1L);
         userBO.delete(user);
         User user2 = userBO.findById(1L);
